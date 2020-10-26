d6652fdf9c4e23d6e885d4652146568bbe1e3b25
Ernestas Ščevinskas <ernestas20111@gmail.com>
2020-10-26 17:18:21 +0200

    Add files via upload

---
+++ PCE_Web/PCE_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Price_Comparison_Engine_Web_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
