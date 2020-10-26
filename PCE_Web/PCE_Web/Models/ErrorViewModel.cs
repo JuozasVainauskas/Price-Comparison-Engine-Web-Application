ab84e4764a0fdf2abf5504f3ae6505bac11bb3a4
Ernestas Ščevinskas <ernestas20111@gmail.com>
2020-10-26 17:21:29 +0200

    Add files via upload

---
+++ PCE_Web/PCE_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PCE_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
