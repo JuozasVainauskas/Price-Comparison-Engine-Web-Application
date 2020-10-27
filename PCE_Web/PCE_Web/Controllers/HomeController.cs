de73faed04dd2604eb98c829b55afa3339f09681
JuozasVainauskas <vainauskasjuozas@gmail.com>
2020-10-27 22:28:42 +0200

    Sutvarkytas dizaino kodas ir pridėtas "Apie" langas"
    Sutvarkytas dizaino kodas ir pridėtas "Apie" langas"

--- PCE_Web/PCE_Web/Controllers/HomeController.cs
+++ PCE_Web/PCE_Web/Controllers/HomeController.cs
@@ -34,7 +34,7 @@ namespace PCE_Web.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public IActionResult ApieLangas()
+        public IActionResult Apie()
         {
             return View();
         }
