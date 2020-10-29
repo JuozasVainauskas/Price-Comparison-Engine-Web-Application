2ecf51c69b4d850d59432325bcacf4319e49bcd2
JuozasVainauskas <vainauskasjuozas@gmail.com>
2020-10-29 11:33:29 +0200

    Pridėtas laikinas prekių ieškojimo langas
    Pridėtas laikinas prekių ieškojimo langas

---
+++ PCE_Web/PCE_Web/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PCE_Web.Models;
+
+namespace PCE_Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Suggestions()
+        {
+            var suggestionsView= new SuggestionsView();
+            suggestionsView.message = "Artimiausiu metu čia galėsite pamatyti siūlomas prekes.";
+            return View(suggestionsView);
+        }
+    }
+}
---
+++ PCE_Web/PCE_Web/Models/SuggestionsView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PCE_Web.Models
+{
+    public class SuggestionsView
+    {
+        public string message { get; set; }
+    }
+}
