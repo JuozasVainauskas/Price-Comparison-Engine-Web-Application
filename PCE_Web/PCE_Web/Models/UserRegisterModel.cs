9f8206e500dd9af37f0ec3c16d04f963fb351d9e
Ernestas Ščevinskas <ernestas20111@gmail.com>
2020-10-31 18:47:50 +0200

    Updates | v1

---
+++ PCE_Web/PCE_Web/Models/UserRegisterModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCE_Web.Models
+{
+    public class UserRegisterModel
+    {
+        [Display]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+    }
+}
