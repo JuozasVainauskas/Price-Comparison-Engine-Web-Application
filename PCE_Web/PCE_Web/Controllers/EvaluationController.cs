f7cb7f9f29d6cc4dbbe3c93b2820e96501b9a971
JustasVit <71339306+JustasVit@users.noreply.github.com>
2020-11-01 09:31:32 +0200

    Pridėtas parduotuvių įvertinimo langas
    Pridėtas parduotuvių įvertinimo langas

---
+++ PCE_Web/PCE_Web/Controllers/EvaluationController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PCE_Web.Controllers
+{
+    public class EvaluationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Evaluate()
+        {
+            return View();
+        }
+    }
+}
