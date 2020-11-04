d86d79736f3388729364cc9be2f4e4a4d6546307
JustasVit <71339306+JustasVit@users.noreply.github.com>
2020-11-04 18:51:56 +0200

    Pridėtas slideshow view
    Pridėtas slideshow view.

---
+++ PCE_Web/PCE_Web/Models/SlideshowView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PCE_Web.Classes;
+
+namespace PCE_Web.Models
+{
+    public class SlideshowView : IEnumerable
+    {
+        public List<Item> Products { get; set; }
+        public IEnumerator GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
