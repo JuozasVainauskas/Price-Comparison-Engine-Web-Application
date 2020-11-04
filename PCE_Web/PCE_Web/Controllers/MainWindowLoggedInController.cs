e75f53c89d52a3d61cc59f50a31526b62ed96b4d
JustasVit <71339306+JustasVit@users.noreply.github.com>
2020-11-04 20:00:27 +0200

    Bug taisymas

--- PCE_Web/PCE_Web/Controllers/MainWindowLoggedInController.cs
+++ PCE_Web/PCE_Web/Controllers/MainWindowLoggedInController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Internal;
 using PCE_Web.Classes;
 using PCE_Web.Models;
 
@@ -15,8 +11,31 @@ namespace PCE_Web.Controllers
     {
         public IActionResult Items()
         {
-                return View();
-        }
+            if (DatabaseManager.ReadSearchedItems().Any())
+            {
+                var products = new List<Item>();
+                foreach (var item in DatabaseManager.ReadSearchedItems())
+                {
+                    products.Add(item);
+                }
+                var slideshowView = new SlideshowView
+                {
+                    Products = products
+                };
+                return View(slideshowView);
+            }
+            else
+            {
+                var products = new List<Item>();
+                var notExistingItem = new Item { Link = "", Picture = "~/img/suggestions/1.jpg", Seller = "", Name = "", Price = "" };
+                products.Add(notExistingItem);
 
+                var slideshowView = new SlideshowView
+                {
+                    Products = products
+                };
+                return View(slideshowView);
+            }
+        }
     }
 }
