a4f1851455cf2d1cafa23dd0faad4f44d9bc8a99
AivarasRinkevicius <71339290+AivarasRinkevicius@users.noreply.github.com>
2020-11-06 19:56:34 +0200

    Pridėtos išsaugotos prekės į MainLoggedInWindow ir jų validacija pagal email, tik neužsideda stylius table

--- PCE_Web/PCE_Web/Controllers/MainWindowLoggedInController.cs
+++ PCE_Web/PCE_Web/Controllers/MainWindowLoggedInController.cs
@@ -13,18 +13,23 @@ namespace PCE_Web.Controllers
         public IActionResult Items(string email)
         {
             emaill = email;
-                if (DatabaseManager.ReadSlidesList().Any())
+
+            if (DatabaseManager.ReadSlidesList().Any())
                 {
                         var products = DatabaseManager.ReadSlidesList();
+                        var productsSaved = DatabaseManager.ReadSavedItems(emaill);
                         var slideshowView = new SlideshowView
                         {
-                            Products = products
+                            Products = products,
+                            ProductsSaved = productsSaved
                         };
+
                         return View(slideshowView);
                 }
                 else
                 {
                         var products = new List<Slide>();
+                        var productsSaved = DatabaseManager.ReadSavedItems(emaill);
                         var notExistingItem = new Slide
                         {
                             PageUrl = "", ImgUrl = "~/img/suggestions/1.jpg"
@@ -33,7 +38,8 @@ namespace PCE_Web.Controllers
 
                         var slideshowView = new SlideshowView
                         {
-                            Products = products
+                            Products = products,
+                            ProductsSaved = productsSaved
                         };
                         return View(slideshowView);
                 }
--- PCE_Web/PCE_Web/Models/SlideshowView.cs
+++ PCE_Web/PCE_Web/Models/SlideshowView.cs
@@ -7,6 +7,7 @@ namespace PCE_Web.Models
     public class SlideshowView : IEnumerable
     {
         public List<Slide> Products { get; set; }
+        public List<Item> ProductsSaved { get; set; }
         public IEnumerator GetEnumerator()
         {
             throw new System.NotImplementedException();
