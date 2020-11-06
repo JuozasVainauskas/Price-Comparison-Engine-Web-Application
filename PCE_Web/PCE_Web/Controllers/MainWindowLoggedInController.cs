34772846e6f6f62dd7806fe3dc1286ff507ba4ca
AivarasRinkevicius <71339290+AivarasRinkevicius@users.noreply.github.com>
2020-11-06 21:10:06 +0200

    Pridėtas prekių ištrinima išsaugotų prisijungus lange

--- PCE_Web/PCE_Web/Controllers/MainWindowLoggedInController.cs
+++ PCE_Web/PCE_Web/Controllers/MainWindowLoggedInController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PCE_Web.Classes;
 using PCE_Web.Models;
@@ -10,39 +11,54 @@ namespace PCE_Web.Controllers
     public class MainWindowLoggedInController : Controller
     {
         public static string emaill = "";
-        public IActionResult Items(string email)
+        public async Task<IActionResult> Items(string email, string link, string pictureUrl, string seller, string name, string price)
         {
-            emaill = email;
+            if (email != null)
+            {
+                emaill = email;
+            }
 
+            if (link != null)
+            {
+                var productToDelete = new Item
+                {
+                    Link = link,
+                    Picture = pictureUrl,
+                    Seller = seller,
+                    Name = name,
+                    Price = price
+                };
+                DatabaseManager.DeleteSavedItem(emaill, productToDelete);
+            }
             if (DatabaseManager.ReadSlidesList().Any())
+            {
+                var products = DatabaseManager.ReadSlidesList();
+                var productsSaved = DatabaseManager.ReadSavedItems(emaill);
+                var slideshowView = new SlideshowView
                 {
-                        var products = DatabaseManager.ReadSlidesList();
-                        var productsSaved = DatabaseManager.ReadSavedItems(emaill);
-                        var slideshowView = new SlideshowView
-                        {
-                            ProductsSaved = productsSaved,
-                            Products = products
-                        };
+                    ProductsSaved = productsSaved,
+                    Products = products
+                };
 
-                        return View(slideshowView);
-                }
-                else
+                return View(slideshowView);
+            }
+            else
+            {
+                var products = new List<Slide>();
+                var productsSaved = DatabaseManager.ReadSavedItems(emaill);
+                var notExistingItem = new Slide
                 {
-                        var products = new List<Slide>();
-                        var productsSaved = DatabaseManager.ReadSavedItems(emaill);
-                        var notExistingItem = new Slide
-                        {
-                            PageUrl = "", ImgUrl = "~/img/suggestions/1.jpg"
-                        };
-                        products.Add(notExistingItem);
+                    PageUrl = "", ImgUrl = "~/img/suggestions/1.jpg"
+                };
+                products.Add(notExistingItem);
 
-                        var slideshowView = new SlideshowView
-                        {
-                            Products = products,
-                            ProductsSaved = productsSaved
-                        };
-                        return View(slideshowView);
-                }
+                var slideshowView = new SlideshowView
+                {
+                    Products = products,
+                    ProductsSaved = productsSaved
+                };
+                return View(slideshowView);
+            }
         }
     }
 }
