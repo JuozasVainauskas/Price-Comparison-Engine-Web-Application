f1634c3ffa67b2ce51950eb0442653df2fe187e9
JustasVit <71339306+JustasVit@users.noreply.github.com>
2020-11-06 09:14:56 +0200

    Komentarų ikėlimas iš duomenų bazės.
    Padarytas komentarų ikėlimas iš duomenų bazės.

--- PCE_Web/PCE_Web/Controllers/EvaluationController.cs
+++ PCE_Web/PCE_Web/Controllers/EvaluationController.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PCE_Web.Classes;
+using PCE_Web.Models;
 
 namespace PCE_Web.Controllers
 {
@@ -14,7 +14,41 @@ namespace PCE_Web.Controllers
         }
         public IActionResult Evaluate()
         {
-            return View();
+            if (DatabaseManager.ReadComments().Any())
+            {
+                var comments = DatabaseManager.ReadComments();
+                var evaluationView = new EvaluationView
+                {
+                    Comments = comments
+                };
+                return View(evaluationView);
+            }
+            else
+            {
+                var comments = new List<CommentsTable>();
+                var notExistingComment = new CommentsTable
+                {
+                    Email = "info@smartshop.lt",
+                    ShopId = 0,
+                    Date = "",
+                    Rating = 0,
+                    Comment = "Komentarų dar nėra",
+                    CommentId = 0
+                };
+                comments.Add(notExistingComment);
+                comments.Add(notExistingComment);
+                comments.Add(notExistingComment);
+                comments.Add(notExistingComment);
+                comments.Add(notExistingComment);
+                comments.Add(notExistingComment);
+                comments.Add(notExistingComment);
+
+                var evaluationView = new EvaluationView
+                {
+                    Comments = comments
+                };
+                return View(evaluationView);
+            }
         }
     }
 }
