6e0f41b9af3bea98cc20e966b1afb2b425f5b3e5
JustasVit <71339306+JustasVit@users.noreply.github.com>
2020-11-06 08:14:50 +0200

    Pridėtas evaluation view.
    Pridėtas evaluation view.

---
+++ PCE_Web/PCE_Web/Models/EvaluationView.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PCE_Web.Classes;
+
+namespace PCE_Web.Models
+{
+    public class EvaluationView : IEnumerable
+    {
+        public List<CommentsTable> Comments { get; set; }
+        public IEnumerator GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
