2f0791c0a3f2fc11394451194fb13976543a710c
JustasVit <71339306+JustasVit@users.noreply.github.com>
2020-11-09 09:52:53 +0200

    Sutvarkyta komentarų rašymo logika

--- PCE_Web/PCE_Web/Controllers/EvaluationSuccessController.cs
+++ PCE_Web/PCE_Web/Controllers/EvaluationSuccessController.cs
@@ -10,17 +10,19 @@ namespace PCE_Web.Controllers
     public class EvaluationSuccessController : Controller
     {
 
-        public IActionResult Success(string comment)
+        public IActionResult Success(int shopId, int rate,string comment)
         {
             
             var currentEmail = MainWindowLoggedInController.EmailCurrentUser;
 
             if (!DatabaseManager.IsAlreadyCommented(currentEmail,shopId) && currentEmail!=null && comment!= null)
             {
-                DatabaseManager.WriteComments(currentEmail, shopId, rating, comment);
+                DatabaseManager.WriteComments(currentEmail, shopId, rate, comment);
             }
             
             Console.WriteLine(comment);
+            Console.WriteLine(rate);
+            Console.WriteLine(shopId);
             return View();
         }
     }
