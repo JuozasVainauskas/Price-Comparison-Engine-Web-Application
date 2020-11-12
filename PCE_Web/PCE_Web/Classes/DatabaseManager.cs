9f4dbf61db3f25237f60aba55870ed625a902760
Ernestas Ščevinskas <ernestas20111@gmail.com>
2020-11-12 18:57:19 +0200

    Update DatabaseManager.cs

--- PCE_Web/PCE_Web/Classes/DatabaseManager.cs
+++ PCE_Web/PCE_Web/Classes/DatabaseManager.cs
@@ -27,7 +27,7 @@ namespace PCE_Web.Classes
 
         private static bool PasswordVerification(string password)
         {
-            var pattern = new Regex(@"(?=.*[a-zA-Z].*[a-zA-Z].*[a-zA-Z].*)(?=.\d.*)[a-zA-Z0-9._-]{3,}",
+            var pattern = new Regex(@"(?=(?:.*[a-z]){3})(?:.*\d)",
                     RegexOptions.Compiled);
             if (string.IsNullOrWhiteSpace(password))
             {
