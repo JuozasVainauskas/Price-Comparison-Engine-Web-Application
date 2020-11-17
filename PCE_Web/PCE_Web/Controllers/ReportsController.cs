c6e69b24c099075a43bdab12b1c731f37447287d
JustasVit <71339306+JustasVit@users.noreply.github.com>
2020-11-17 11:28:59 +0200

    Created reports controller, model and view

---
+++ PCE_Web/PCE_Web/Controllers/ReportsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PCE_Web.Classes;
+using PCE_Web.Models;
+
+namespace PCE_Web.Controllers
+{
+    [Authorize]
+    public class ReportsController : Controller
+    {
+        private readonly IDatabaseManager _databaseManager;
+        public ReportsController(IDatabaseManager databaseManager)
+        {
+            _databaseManager = databaseManager;
+        }
+        public IActionResult Report(string email = "")
+        {
+            return View();
+        }
+    }
+}
---
+++ PCE_Web/PCE_Web/Models/ReportView.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PCE_Web.Classes;
+
+namespace PCE_Web.Models
+{
+    public class AdminView : IEnumerable
+    {
+        public List<User> Users { get; set; }
+
+        public List<User> ReportedUsers { get; set; }
+        public List<Exceptions> Exceptions { get; set; }
+        public string Role;
+
+        public IEnumerator GetEnumerator()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
