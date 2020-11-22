9fe2842c2b550ca9dc685b23d0ce2579a38e0a98
JustasVit <71339306+JustasVit@users.noreply.github.com>
2020-11-22 11:48:29 +0200

    answerReportMessage metodas

--- PCE_Web/PCE_Web/Classes/EmailSender.cs
+++ PCE_Web/PCE_Web/Classes/EmailSender.cs
@@ -44,6 +44,52 @@ namespace PCE_Web.Classes
             client.SendCompleted += ClientSendCompleted;
             await client.SendMailAsync(message);
         }
+        public static async void answerReportMessage(string email, int identifier, string messageText = "")
+        {
+            var client = new SmtpClient()
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+
+                Credentials = new NetworkCredential()
+                {
+                    UserName = "smartshopautobot@gmail.com",
+                    Password = "adminNull0"
+                }
+            };
+
+            var fromEmail = new MailAddress("smartshopautobot@gmail.com", "Smart Shop");
+            var toEmail = new MailAddress(email, "Naudotojas");
+            if (identifier == 0)
+            {
+                var message = new MailMessage()
+                {
+                    IsBodyHtml = true,
+                    From = fromEmail,
+                    Subject = "Atsakymas į pranešimą",
+                    Body = "Sveiki,<br>administratorius perskaitė Jūsų pranešimą ir norėtų tęsti bendradarbiavimą šalinant klaidas/įgyvendinant pasiūlymus. <br><b>Administratoriaus pranešimas:</b> <br><br> " + messageText + " <br><img src=\"https://i.pinimg.com/originals/d4/2a/8c/d42a8c4e83f0fb3750af810be2abbb23.png\" alt =\"SmartShop\" width=\"50\" height=\"50\"><br><i>Pasirašo,<br>Smart Shop komanda.</i>"
+                };
+                message.To.Add(toEmail);
+                client.SendCompleted += ClientSendCompleted;
+                await client.SendMailAsync(message);
+            }
+            else
+            {
+                var message = new MailMessage()
+                {
+                    IsBodyHtml = true,
+                    From = fromEmail,
+                    Subject = "Atsakymas į pranešimą",
+                    Body = "Sveiki,<br>dėkojame už pranešimą, paliktą mūsų svetainėje.Jei jūs nesinaudojote mūsų paslaugomis ir nieko nepranešėte, prašome ignoruoti šį laišką.<br><img src=\"https://i.pinimg.com/originals/d4/2a/8c/d42a8c4e83f0fb3750af810be2abbb23.png\" alt =\"SmartShop\" width=\"50\" height=\"50\"><br><i>Pasirašo,<br>Smart Shop komanda.</i>"
+                };
+                message.To.Add(toEmail);
+                client.SendCompleted += ClientSendCompleted;
+                await client.SendMailAsync(message);
+            }
+        }
 
         private static void ClientSendCompleted(object sender, AsyncCompletedEventArgs e)
         {
