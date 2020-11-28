e90ab968a491d5100247ca0151cc9428e52893c3
JuozasVainauskas <vainauskasjuozas@gmail.com>
2020-11-28 22:05:58 +0200

    Pridėta mobili aplikacija.
    Pridėta mobili aplikacija.

---
+++ PCE_Web/PCE_App/PCE_App/Views/ItemsPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using PCE_App.Models;
+using PCE_App.Views;
+using PCE_App.ViewModels;
+
+namespace PCE_App.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
---
+++ PCE_Web/PCE_App/PCE_App/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using PCE_App.Models;
+using PCE_App.ViewModels;
+
+namespace PCE_App.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
